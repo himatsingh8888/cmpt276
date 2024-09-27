497daac5c086fd4f0ea0ade6fa96bc3e0f9ebce0
Damin <daminmutti@icloud.com>
2024-09-26 23:22:42 -0700

    Resolved merge conflict in review.rb

--- ratemygym/app/models/review.rb
+++ ratemygym/app/models/review.rb
@@ -1,9 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :gym
-<<<<<<< HEAD
-=======
   belongs_to :user
->>>>>>> reviews
 
   validates :comment, presence: true
   validates :rating, presence: true, inclusion: { in: 1..5 }
