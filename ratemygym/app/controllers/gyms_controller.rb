63fb019214b9e23260ee93d7bfb8bd7549d7d843
gurtajrattan <113313666+gurtajrattan@users.noreply.github.com>
2024-10-26 16:00:42 -0700

    Created an average review section which calculates the average reviews ratings and displays them

--- ratemygym/app/controllers/gyms_controller.rb
+++ ratemygym/app/controllers/gyms_controller.rb
@@ -2,5 +2,12 @@ class GymsController < ApplicationController
   def show
     @gym = Gym.find(params[:id])
     @reviews = @gym.reviews.order(created_at: :desc)
+
+     # Calculate averages of each section rounded to one decimal place
+     @average_machines_rating = @reviews.average(:rating).to_f.round(1)  
+     @average_staff_rating = @reviews.average(:staff_rating).to_f.round(1)
+     @average_location_rating = @reviews.average(:location_rating).to_f.round(1)
+     @average_cleanliness_rating = @reviews.average(:clean_rating).to_f.round(1)
+     @average_atmosphere_rating = @reviews.average(:atmosphere_rating).to_f.round(1)
   end
 end
--- ratemygym/app/helpers/reviews_helper.rb
+++ ratemygym/app/helpers/reviews_helper.rb
@@ -1,2 +1,36 @@
 module ReviewsHelper
+    #Helper function to 
+    def star_rating_display(average_rating)
+         # Return empty stars if average_rating is nil
+        return ('<i class="bx bx-star" style="color: lightgray; font-size: 1.6rem;"></i>' * 5).html_safe if average_rating.nil?
+        # Calculates the total amount of full stars
+        integer_part = average_rating.floor
+        # Finds the fraction amound to display half star
+        fractional_part = average_rating - integer_part
+
+        # Determine the number of remaining stars
+        remaining_stars = 5 - integer_part
+
+        # Generate full stars
+        full_stars = ('<i class="bx bxs-star" style="color: #FFBD13; font-size: 1.6rem;"></i>' * integer_part).html_safe
+        if fractional_part == 0.8 || fractional_part == 0.9
+            star = ('<i class="bx bxs-star" style="color: #FFBD13; font-size: 1.6rem;"></i>' * integer_part).html_safe
+            full_stars + star
+        end
+
+        #Generate half stars 
+        half_star = ''
+        # only generates half stars when the fractional amount is between 0.3 and 0.8
+        if fractional_part >= 0.3 && fractional_part <= 0.7
+        remaining_stars -= 1
+        half_star = '<i class="bx bxs-star-half" style="color: #FFBD13; font-size: 1.6rem;"></i>'.html_safe
+    
+        end
+
+        # Generate empty stars
+        empty_stars = ('<i class="bx bx-star" style="color: lightgray; font-size: 1.6rem;"></i>' * remaining_stars).html_safe
+
+        # Return the combined HTML for full, half, and empty stars
+        full_stars + half_star + empty_stars
+    end
 end
