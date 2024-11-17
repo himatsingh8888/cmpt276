2258e18c9958fc9dabe465209ce03e45520cf101
Himat Singh Kahlon <himatsingh8888@gmail.com>
2024-11-17 14:36:33 -0800

    trying to make the render work

--- ratemygym/app/models/gym.rb
+++ ratemygym/app/models/gym.rb
@@ -1,3 +1,4 @@
 class Gym < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :image_url, presence: true  # Optionally validate the image_url field
 end
--- ratemygym/db/migrate/20240926215018_create_gyms.rb
+++ ratemygym/db/migrate/20240926215018_create_gyms.rb
@@ -3,6 +3,7 @@ class CreateGyms < ActiveRecord::Migration[7.2]
     create_table :gyms do |t|
       t.string :name
       t.string :location
+      t.string :image_url
 
       t.timestamps
     end
